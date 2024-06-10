b116c981a272c7cb728c86d0ca8cc64d78cdaf58
haadisaqib <haadi.saqib@gmail.com>
2024-06-10 10:38:46 -0500

    update

---
+++ week-2/lab3-1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+    int num = 10;
+    std::cout << "num = " << num << '\n';
+
+    //arthemitic example
+    num = num + 1;
+    std::cout << "num + 1= " << num << '\n';
+
+    //floating point example
+    float num2 = 10.12312;
+    std::cout << "num2 = " << num2 << '\n';
+
+    //Character example
+    char letter = 'a';
+    std::cout << "letter = " << letter << '\n';
+
+}
+

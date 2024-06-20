db4d72c0ae6dbc59f25a998145c57f9b1a1acdcb
haadisaqib <haadi.saqib@gmail.com>
2024-06-20 12:57:32 -0500

    update

---
+++ week-3/unit-7/forloop.cpp
@@ -0,0 +1,13 @@
+//for loop example
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i;
+    for (i = 0; i < 5; i++)
+    {
+        cout << "Hello World\n";
+    }
+    return 0;
+}
+
